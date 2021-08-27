03ada2e06ff520ad83a49e655fe669460661df0d
egzon2 <egzonthaqi99@gmail.com>
2021-08-27 23:26:56 +0200

    Initial commit

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Activities;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await Mediator.Send(new List.Query());
+        }
+
+        [HttpGet("{id}")] //activities/id
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await Mediator.Send(new Details.Query{Id = id});  //nese activity nuk ekziston na kthen null prej Mediator(null nuk eshte error, po kthen no content)
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity)  //kthen akces ne Http response types si: ok, return bad request, return not found...
+        {
+            return Ok(await Mediator.Send(new Create.Command {Activity = activity}));
+        }    
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        {
+            activity.Id = id;
+            return Ok(await Mediator.Send(new Edit.Command{Activity = activity}));  //me update veq ni single field "Id" ne kete rast
+        }     
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(Guid id)
+        {
+            return Ok(await Mediator.Send(new Delete.Command{Id = id}));
+        } 
+    }
+}
---
+++ Application/Activities/Create.cs
@@ -0,0 +1,35 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class Create
+    {
+        public class Command : IRequest
+        {
+            public Activity Activity { get; set; }
+        }
+
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                _context.Activities.Add(request.Activity); //adding activity ne context ne memory
+
+                await _context.SaveChangesAsync();
+
+                return Unit.Value;  //letting API Controller know that we finished what is going inside here
+            }
+        }
+    }
+}
