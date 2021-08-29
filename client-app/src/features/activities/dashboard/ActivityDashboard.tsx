328221aec0c289c0e30f9c49b01bd5f6986f58f8
egzon2 <egzonthaqi99@gmail.com>
2021-08-29 19:40:12 +0200

    commit

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,25 +1,25 @@
 import { observer } from 'mobx-react-lite';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Grid } from 'semantic-ui-react';
 import { useStore } from '../../../app/api/stores/store';
-import ActivityDetails from '../details/ActivityDetails';
-import ActivityForm from '../form/ActivityForm';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
 import ActivityList from './ActivityList';
 
-export default observer(function ActivityDashboard() {
-        
+export default observer(function ActivityDashboard() {     
+    
     const {activityStore} = useStore();
-    const {selectedActivity, editMode} = activityStore;
+    useEffect(() => {
+      activityStore.loadActivities();
+    }, [activityStore])
+  
+  if (activityStore.loadingInitial) return <LoadingComponent content='Loading app'/>
     return (
         <Grid>
             <Grid.Column width='10'>
                 <ActivityList/>
             </Grid.Column>
             <Grid.Column width='6'>
-                {selectedActivity && !editMode &&
-                <ActivityDetails />}
-                {editMode &&
-                <ActivityForm />}
+                <h2>Activity filters</h2>
             </Grid.Column>
         </Grid>
     )
--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -3,13 +3,21 @@ import { Button, Card, Image } from 'semantic-ui-react';
 import { useStore } from '../../../app/api/stores/store';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import './culture.jpg';
+import { useParams } from 'react-router';
+import { useEffect } from 'react';
+import { observer } from 'mobx-react-lite';
 
-export default function ActivityDetails(){
+export default observer(function ActivityDetails(){
 
     const {activityStore} = useStore();
-    const {selectedActivity: activity, openForm, cancelSelectedActivity} = activityStore;
+    const {selectedActivity: activity, loadActivity, loadingInitial} = activityStore;
+    const {id} = useParams<{id: string}>();
 
-    if (!activity) return <LoadingComponent/>;
+    useEffect(() => {
+        if(id) loadActivity(id);
+    }, [id, loadActivity]);
+
+    if (loadingInitial || !activity) return <LoadingComponent/>;
     return (
         <Card fluid>
             <Image src={'/assets/categoryImages/culture.jpg'}/>
@@ -24,10 +32,10 @@ export default function ActivityDetails(){
             </Card.Content>
             <Card.Content extra>
                 <Button.Group widths='2'>
-                    <Button onClick={() => openForm(activity.id)}basic color='blue' content='Edit'/>
-                    <Button onClick={cancelSelectedActivity} basic color='grey' content='Cancel'/>
+                    <Button  basic color='blue' content='Edit'/>
+                    <Button  basic color='grey' content='Cancel'/>
                 </Button.Group>
             </Card.Content>
         </Card>
     )
-}
+})
